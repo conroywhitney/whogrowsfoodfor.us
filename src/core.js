ffbafd056bc79fca55dfc860545c49e567986804
Conroy Whitney <conroy.whitney@gmail.com>
2015-10-15 16:20:06 -0700

    core -- zoomXYZ -- TODO note

--- src/core.js
+++ src/core.js
@@ -10,6 +10,7 @@ export function select(state, fips) {
 
 export function getZoomXYZ(zoomFIPS) {
   if(!zoomFIPS) { return null; }
+  // TODO: once have UI, find out boundary coordinates of path for FIPS
   return List([1, 2, 3]);
 }
 
