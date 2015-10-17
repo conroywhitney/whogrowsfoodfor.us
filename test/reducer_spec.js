47ee3eb620cfed459c04f05517d8fbab73017bcf
Conroy Whitney <conroy.whitney@gmail.com>
2015-10-17 14:05:31 -0700

    reducer -- handles SELECT at basic level

--- test/reducer_spec.js
+++ test/reducer_spec.js
@@ -5,13 +5,20 @@ import {INITIAL_STATE} from '../src/core';
 
 describe('reducer', () => {
 
+  const countyFIPS   = '41029';
+
 	it('has an initial state', () => {
     const nextState = reducer(null, null);
     expect(nextState).to.equal(INITIAL_STATE);
   });
 
-	xit('handles SELECT', () => {
+	it('handles SELECT at basic level', () => {
+		const action    = {type: 'SELECT', fips: countyFIPS};
+		const nextState = reducer(INITIAL_STATE, action);
 
+		// simplest thing to make this pass is checking 'selected'
+		// checking other attributes of state will make this too brittle
+		expect(nextState.get('selected')).to.equal(countyFIPS);
 	});
 
 });
