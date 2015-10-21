5078c610d31a6f68a8f90d04c56a439312756de2
Conroy Whitney <conroy.whitney@gmail.com>
2015-10-21 00:15:13 -0700

    gulp -- diggin in deep to geography download

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,42 @@
 var
-  gulp     = require('gulp-param')(require('gulp'), process.argv),
-  download = require('gulp-download'),
-  rename   = require('gulp-rename')
+  gulp         = require('gulp-param')(require('gulp'), process.argv),
+  download     = require('gulp-download'),
+  replace      = require('gulp-replace'),
+  insert       = require('gulp-insert'),
+  csv          = require('gulp-csv2json'),
+  jsonRefactor = require('gulp-json-refactor'),
+  rename       = require('gulp-rename'),
+  TEMPDIR      = './data/raw'
 ;
 
-gulp.task('download', function(url, filename) {
-  download(url)
-    .pipe(rename(filename))
-    .pipe(gulp.dest('./'))
-  ;
+var geography = [
+  {
+    url: 'http://www2.census.gov/geo/docs/reference/codes/files/national_county.txt',
+    filename: 'counties.csv',
+    csv: {
+      delimiter: ','
+    },
+    refactor: {
+    }
+  },
+  {
+    url: 'http://www2.census.gov/geo/docs/reference/state.txt',
+    filename: 'states.csv',
+    csv: {
+      delimiter: '|'
+    },
+    refactor: {
+    }
+  }
+];
+
+gulp.task('geography', function() {
+  geography.forEach(function(obj) {
+    download(obj.url)
+      .pipe(rename(obj.filename))
+      .pipe(csv(obj.csv))
+      .pipe(rename({extname: '.json'}))
+      .pipe(gulp.dest(TEMPDIR))
+  });
 });
 
