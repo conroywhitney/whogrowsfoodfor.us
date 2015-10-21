ce702a64e9f21b49a5269b45f8b57b1a7be88a4e
Conroy Whitney <conroy.whitney@gmail.com>
2015-10-21 09:38:24 -0700

    products -- filters and tests

--- src/products.js
+++ src/products.js
@@ -3,9 +3,30 @@ import {INITIAL_STATE} from './constants'
 
 export const productJSON = INITIAL_STATE.getIn(['data', 'products']).toJS();
 
+export const filteredProducts = filterProducts(productJSON);
+
 export const productList = Object.keys(productJSON);
 
 export const productOptions = productList.map(p =>
                                 ({value: p, label: productJSON[p].name})
                               );
 
+
+export function filterProducts(arr) {
+  // return blank object if null or empty array
+  if(!arr || arr.length == 0) { return [] };
+
+  // return same object if not an array
+  if(!Array.isArray(arr)) { return arr; }
+
+  return arr.filter(productFilter);
+}
+
+export function productFilter(product) {
+  var
+    regex  = new RegExp('OTHER|TOTALS|REPAIRS|INTERNET|AREA|HYBRIDS|TAXES|OPERATIONS|LABOR|FARM|LAND|SPAWN|FACILITY|SOD|LOANS|ENERGY|INTEREST|PRINCIPAL|DEPRECIATION|OPERATORS|SERVICES|PROGRAMS|PRACTICES|RENT|FUELS|ORNAMENTAL FISH|TREES & SHORT|FEED|HAYLAGE|PIGEONS|TEMPLES', 'gi'),
+    ignore = regex.test(product),
+    keep   = (ignore == false)
+  ;
+  return keep;
+}
--- test/products_spec.js
+++ test/products_spec.js
@@ -1,7 +1,7 @@
 import {expect} from 'chai';
 import {Map, List, fromJS} from 'immutable';
 
-import {productJSON, productList, productOptions, sectors} from '../src/products'
+import {productJSON, productList, productOptions, sectors, filterProducts, productFilter} from '../src/products'
 
 describe('products', () => {
 
@@ -35,6 +35,52 @@ describe('products', () => {
 
     it('should have the same number of options', () => {
       expect(productOptions.length).to.eq(NUM_PRODUCTS);
+    });
+
+  });
+
+  describe('filterProducts', () => {
+
+    var
+      desirable   = ['ASPARAGUS', 'EGGPLANT', 'SPINACH'],
+      undesirable = ['RENT', 'INTEREST', 'FARM LOANS'],
+      combined    = desirable.concat(undesirable)
+    ;
+
+    it('should return a blank array if null', () => {
+      expect(filterProducts(null)).to.eql([]);
+    });
+
+    it('should return a blank array if blank array', () => {
+      expect(filterProducts([])).to.eql([]);
+    });
+
+    it('should return the same object if not an array', () => {
+      expect(filterProducts({test:'value'})).to.eql({test:'value'});
+    });
+
+    it('should return the same array if no undesirable words', () => {
+      expect(filterProducts(desirable)).to.eql(desirable);
+    });
+
+    it('should return a filtered array if undesirable words', () => {
+      expect(filterProducts(combined)).to.eql(desirable);
+    });
+
+    it('should return a blank array if all undesirable words', () => {
+      expect(filterProducts(undesirable)).to.eql([]);
+    });
+
+  });
+
+  describe('productFilter', () => {
+
+    it('should keep desirable words', () => {
+      expect(productFilter("SQUASH")).to.be.true;
+    });
+
+    it('should ignore undesirable words', () => {
+      expect(productFilter("PIGEONS")).to.be.false;
     });
 
   });
