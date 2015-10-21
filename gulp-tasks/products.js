e19cf05b1251db2c38012e4b0c984abf0b55cc76
Conroy Whitney <conroy.whitney@gmail.com>
2015-10-21 12:00:18 -0700

    gulp -- refactor -- prep for geo

--- gulp-tasks/products.js
+++ gulp-tasks/products.js
@@ -10,10 +10,36 @@ var
   jsonRefactor = require('gulp-json-refactor'),
   rename       = require('gulp-rename'),
   slug         = require('slug'),
+  concat       = require('gulp-concat'),
   TEMPDIR      = './tmp/',
   RAWDIR       = './data/raw/',
   DATADIR      = './data/'
 ;
+
+function getProductList() {
+  var
+    productList = RAWDIR + 'product-list.json',
+    productsRaw = fs.readFileSync(productList),
+    productJSON = JSON.parse(productsRaw),
+    products    = productJSON["data"][0]["Values"]
+  ;
+
+  return products;
+}
+
+function getProductProperties(product) {
+  var
+    productQSVar  = product,
+    productSlug   = slug(productQSVar.toLowerCase(), '_'),
+    productFolder = RAWDIR + productSlug
+  ;
+
+  return {
+    qsVar:  productQSVar,
+    slug:   productSlug,
+    folder: productFolder
+  }
+}
 
 gulp.task('product-list', function() {
   var
@@ -27,24 +53,19 @@ gulp.task('product-list', function() {
 
 gulp.task('product-metadata', function() {
   var
-    productList = RAWDIR + 'product-list.json',
-    productsRaw = fs.readFileSync(productList),
-    productJSON = JSON.parse(productsRaw),
-    products    = productJSON["data"][0]["Values"],
-    options     = ['class_desc', 'statisticcat_desc', 'util_practice_desc', 'prodn_practice_desc', 'unit_desc', 'domain_desc']
+    products = getProductList(),
+    options  = ['class_desc', 'statisticcat_desc', 'util_practice_desc', 'prodn_practice_desc', 'unit_desc', 'domain_desc']
   ;
 
   products.forEach(function(product) {
     var
-      productQSVar  = product,
-      productSlug   = slug(productQSVar.toLowerCase(), '_'),
-      productFolder = RAWDIR + productSlug
+      props = getProductProperties(product)
     ;
 
     options.forEach(function(option) {
       var
-        url      = 'http://nass-api.azurewebsites.net/api/get_dependent_param_values?source_desc=CENSUS&year=2012&freq_desc=ANNUAL&agg_level_desc=COUNTY&distinctParams=' + option + '&commodity_desc=' + product,
-        filename = productSlug + '_' + option + '.json'
+        url      = 'http://nass-api.azurewebsites.net/api/get_dependent_param_values?source_desc=CENSUS&year=2012&freq_desc=ANNUAL&agg_level_desc=COUNTY&distinctParams=' + option + '&commodity_desc=' + props.qsVar,
+        filename = props.slug + '_' + option + '.json'
       ;
 
       download(url)
@@ -53,10 +74,43 @@ gulp.task('product-metadata', function() {
           return json["data"][0]["Values"];
         }))
         .pipe(rename(filename))
-        .pipe(gulp.dest(productFolder))
+        .pipe(gulp.dest(props.folder))
     });
   });
 
-})
+});
+
+gulp.task('product-concat', function() {
+  var
+    products = getProductList()
+  ;
+
+  products.forEach(function(product) {
+    var
+      props = getProductProperties(product)
+    ;
+
+    gulp.src(props.folder + '*.json')
+      .pipe(concat(product + '_options.json'))
+      .pipe(gulp.dest(props.folder))
+
+  });
+
+});
+
+gulp.task('product-values', function() {
+  var
+    products    = getProductList(),
+    geographies = ['NATIONAL', 'STATE', 'COUNTY']
+  ;
+
+  products.forEach(function(product) {
+    var
+      props = getProductProperties(product)
+    ;
+
+  });
+
+});
 
 gulp.task('products', gulpSequence('product-list', 'product-metadata'));
