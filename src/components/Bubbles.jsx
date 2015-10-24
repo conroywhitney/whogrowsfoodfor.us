625492749717711723b8d2a23306ff8e557ea726
Conroy Whitney <conroy.whitney@gmail.com>
2015-10-23 21:49:35 -0700

    bubbles -- radius function

--- src/components/Bubbles.jsx
+++ src/components/Bubbles.jsx
@@ -5,24 +5,30 @@ import {Circle} from 'react-d3';
 export default React.createClass({
   mixins: [PureRenderMixin],
 
+  radius: function(location) {
+    return 2;
+  },
+
   render: function() {
     var
-      d3path   = d3.geo.path(),
-      items = this.props.topoJSON.features
+      d3path    = d3.geo.path(),
+      geography = this.props.topoJSON.features,
+      data      = this.props.data
     ;
     return (
       React.DOM.g({
         className: "bubbles"
       },
-        items.map(function(item) {
+        geography.map(function(location) {
           return React.DOM.circle({
             className: "bubble",
-            r: 2,
-            transform: `translate(${d3path.centroid(item)})`
+            r: this.radius(location),
+            transform: `translate(${d3path.centroid(location)})`
           })
-        })
+        }, this)
       )
     );
   }
+
 });
 
