dbe167bf0569b91fc262a8c4ff42c2cc69b86b8d
Conroy Whitney <conroy.whitney@gmail.com>
2015-10-24 01:02:50 -0700

    hover-click -- refactor -- separate out methods for clarity

--- src/components/ClickableMapLayer.jsx
+++ src/components/ClickableMapLayer.jsx
@@ -14,11 +14,7 @@ export default React.createClass({
     var
       width      = 900,
       height     = 400,
-      d3path     = d3.geo.path().projection(d3.geo.albersUsa()),
-      domElement = React.findDOMNode(this),
       target     = event.target,
-      d3Data     = target.getAttribute('d'),
-      bounds     = target.getAttribute('bounds'),
       fips       = target.id
     ;
 
@@ -26,17 +22,30 @@ export default React.createClass({
     if (this.state.active.node() === target) return this.reset();
 
     // the exciting part -- update global state with fips code!
+    // this method has been passed down from the outermost container
     this.props.setRegion(fips);
 
+    // handle fancy d3 stuff
+    this.d3Highlight(target);
+    //this.d3ZoomIn(target);
+  },
+
+  d3Highlight: function(target) {
     // d3 updates for selecting state outline
     this.state.active.classed("active", false);
     this.setState({
       active: d3.select(target).classed("active", true)
     });
+  },
 
+  d3ZoomIn: function(target) {
     // transform / scale calculations for zooming to bounding box
     // TODO: fix  =(
-    var bounds    = d3path.bounds(d3Data),
+    var width     = 900,
+        height    = 400,
+        d3data    = target.getAttribute('d'),
+        d3path    = d3.geo.path().projection(d3.geo.albersUsa()),
+        bounds    = d3path.bounds(d3data),
         dx        = bounds[1][0] - bounds[0][0],
         dy        = bounds[1][1] - bounds[0][1],
         x         = (bounds[0][0] + bounds[1][0]) / 2,
@@ -52,15 +61,23 @@ export default React.createClass({
   },
 
   reset: function() {
-    this.state.active.classed("active", false);
+    // the exciting part -- update global state to nothing!
+    // this function was passed down from the outermost container
+    this.props.setRegion(null);
 
+    // handle fancy d3 stuff
+    this.d3Unhighlight();
+    //this.d3ZoomOut();
+  },
+
+  d3Unhighlight: function() {
+    this.state.active.classed("active", false);
     this.setState({
       active: d3.select(null)
     });
+  },
 
-    // the exciting part -- update global state to nothing!
-    this.props.setRegion(null);
-
+  d3ZoomOut: function() {
     d3.transition()
         .duration(750)
         .style("stroke-width", "1.5px")
