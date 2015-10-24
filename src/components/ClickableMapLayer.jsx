cfe549294cbdf9f1951dd2bff6f9c9b0ba1e6019
Conroy Whitney <conroy.whitney@gmail.com>
2015-10-23 23:44:39 -0700

    hover-click -- clickable map layer -- getting closer, but infinity bounds

--- src/components/ClickableMapLayer.jsx
+++ src/components/ClickableMapLayer.jsx
@@ -3,26 +3,75 @@ import PureRenderMixin from 'react-addons-pure-render-mixin';
 import d3 from 'd3'
 
 export default React.createClass({
-  mixins: [PureRenderMixin],
 
-  // react findDomNode helper in component#didMount
-  // attach event handler defined here
+  getInitialState: function() {
+    return {
+      active: d3.select(null)
+    }
+  },
 
-  handleClick: function() {
-    console.log('ahndle click');
+  handleClick: function(event) {
+    if (this.state.active.node() === this) return reset();
+
+    var
+      d3path     = d3.geo.path(),
+      domElement = React.findDOMNode(this),
+      innerPath  = domElement.firstChild,
+      d3Data     = innerPath.getAttribute('d'),
+      width      = 900,
+      height     = 400
+    ;
+
+    this.state.active.classed("active", false);
+
+    this.setState({
+      active: d3.select(domElement).classed("active", true)
+    });
+
+    console.log(d3Data);
+
+    var bounds    = d3path.bounds(d3Data),
+        dx        = bounds[1][0] - bounds[0][0],
+        dy        = bounds[1][1] - bounds[0][1],
+        x         = (bounds[0][0] + bounds[1][0]) / 2,
+        y         = (bounds[0][1] + bounds[1][1]) / 2,
+        scale     = .9 / Math.max(dx / width, dy / height),
+        translate = [width / 2 - scale * x, height / 2 - scale * y]
+    ;
+
+    console.log(bounds);
+
+    d3.transition()
+        .duration(750)
+        .style("stroke-width", 1.5 / scale + "px")
+        .attr("transform", "translate(" + translate + ")scale(" + scale + ")");
+  },
+
+  reset: function() {
+    active.classed("active", false);
+    active = d3.select(null);
+
+    g.transition()
+        .duration(750)
+        .style("stroke-width", "1.5px")
+        .attr("transform", "");
   },
 
   render: function() {
     var
-      d3path = d3.geo.path()
+      d3path    = d3.geo.path(),
+      geography = this.props.topoJSON.features
     ;
 
     return (
       React.DOM.g({},
-        React.DOM.path({
-          className: this.props.className,
-          d: d3path(this.props.topoJSON)
-        })
+        geography.map(function(location) {
+          return React.DOM.path({
+            className: this.props.className,
+            d: d3path(location),
+            onClick: this.handleClick
+          })
+        }, this)
       )
     );
   }
