83dde36e18171b10b66163d93fe4f2a4038e5836
Conroy Whitney <conroy.whitney@gmail.com>
2015-10-25 19:28:24 -0700

    data -- add productData constant to products.js

--- src/products.js
+++ src/products.js
@@ -5,6 +5,7 @@ import {getProductLabel} from './label';
 
 export const productMeta    = require('../data/meta/product_meta.json');
 export const productKeys    = require('../data/meta/product_keys.json').keys;
+export const productData    = require('../data/products.json').products;
 export const productOptions = productKeys
                                   .map(key => ({
                                     value: key,
