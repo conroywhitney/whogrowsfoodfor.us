a0d16da622915434e7f88f73dc1bbf1608ae50b4
Conroy Whitney <conroy.whitney@gmail.com>
2015-10-26 16:14:48 -0700

    histogram -- vertical histogram bar -- broke out into own component

--- src/components/VerticalHistogram.jsx
+++ src/components/VerticalHistogram.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import d3 from 'd3';
+import VerticalHistogramBar from './VerticalHistogramBar';
 
 export default React.createClass({
   mixins: [PureRenderMixin],
@@ -24,8 +25,6 @@ export default React.createClass({
       overageLabel   = tooManyItems ? (itemOverage + 1) + ' locations not shown' : '',
       chartWidth     = this.props.width,
       chartHeight    = this.props.height,
-      barWidth       = 24,
-      barPadding     = 4,
       yScaleFunction = d3.scale.linear().range([0, chartHeight]),
       formatFunction = this.numberWithCommas
     ;
@@ -38,50 +37,19 @@ export default React.createClass({
         height: chartHeight,
         className: "chart"
       },
-        sliced.map(function(d, i) {
-          var
-            barHeight      = yScaleFunction(d.value),
-            xOffset        = (i * barWidth) + barWidth - barPadding,  // make sure lines up on far-right without going over
-            xPos           = chartWidth - xOffset,
-            yPos           = chartHeight - barHeight,
-            formattedValue = formatFunction(d.value),
-            groupPosition  = "translate(" + xPos + "," + yPos + ")",
-            labelUnits     = ' acres',
-            barTooShort    = barHeight < 100,
-            label          = formattedValue,
-            labelX         = barTooShort ? 5 : -(barWidth / 2 - 2),
-            labelY         = (barWidth / 2 - 2),
-            labelClass     = barTooShort ? 'exterior' : 'interior',
-            labelRotate    = 270,
-            regionName     = d.label,
-            regionX        = -(barHeight + 5),
-            regionY        = labelY
-          ;
-
+        sliced.map(function(d, barIndex) {
           return (
-            React.DOM.g({
-              transform: groupPosition
-            },
-              React.DOM.rect({
-                className: "bar",
-                width: barWidth - barPadding,
-                height: barHeight
-              }),
-              React.DOM.text({
-                className: "valueLabel " + labelClass,
-                x: labelX,
-                y: labelY,
-                dy: "0.35em",
-                transform: "rotate(" + labelRotate + ")",
-              }, label),
-              React.DOM.text({
-                className: "regionLabel",
-                x: regionX,
-                y: regionY,
-                dy: "0.35em",
-                transform: "rotate(" + labelRotate + ")"
-              }, regionName)
-            )
+            <VerticalHistogramBar
+              value={d.value}
+              label={d.label}
+              barIndex={barIndex}
+              width={24}
+              padding={4}
+              chartWidth={chartWidth}
+              chartHeight={chartHeight}
+              formatFunction={formatFunction}
+              scaleFunction={yScaleFunction}
+            />
           )
         }),
         React.DOM.text({
